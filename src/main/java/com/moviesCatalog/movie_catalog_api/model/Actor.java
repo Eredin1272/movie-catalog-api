3e2c79448dafc0dba5c7349c15833640a1c24d37
Eredin <britcovegor@gmail.com>
2026-03-08 00:42:08 +0200

    only for Vadick and Serij

---
+++ src/main/java/com/moviesCatalog/movie_catalog_api/model/Actor.java
@@ -0,0 +1,23 @@
+package com.moviesCatalog.movie_catalog_api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "actors")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Actor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "name")
+    private String name;
+
+    @Column(nullable = false, name = "birthYear")
+    private Integer birthYear;
+}
---
+++ src/main/java/com/moviesCatalog/movie_catalog_api/model/Movie.java
@@ -0,0 +1,34 @@
+package com.moviesCatalog.movie_catalog_api.model;
+
+import jakarta.persistence.*; // библиотека для работы с базой данных через Java
+import lombok.*;
+
+@Entity // Этот класс является сущностью базы данных
+@Table(name = "movies")
+@Data
+@NoArgsConstructor //Создает пустой конструктор
+@AllArgsConstructor // Создает конструктор со всеми параметрами:
+@Builder // Позволяет удобно создавать объект
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // генерируется ID автоматически
+    private Long id;
+
+    @Column(nullable = false, name = "title")
+    private String title;
+
+    @Column(nullable = false, name = "releaseYear")
+    private Integer releaseYear;
+
+    @Column(nullable = false, name = "rating")
+    private Double rating;
+
+    @ManyToOne // много фильмов - один жанр
+    @JoinColumn(name = "genre_id") // в таблице movies будет колонка genre_id
+    private Genre genre;
+
+    @ManyToOne // много фильмов - один режиссер
+    @JoinColumn(name = "director_id") // // в таблице movies будет колонка director
+    private Director director;
+}
