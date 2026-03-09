1669f69685fc43c6639596ec4887eec4527aa546
Eredin <britcovegor@gmail.com>
2026-03-10 01:27:43 +0200

    only for Vadick and Serij

---
+++ src/main/java/com/moviesCatalog/movie_catalog_api/controller/MovieController.java
@@ -0,0 +1,40 @@
+package com.moviesCatalog.movie_catalog_api.controller;
+
+import com.moviesCatalog.movie_catalog_api.model.Movie;
+import com.moviesCatalog.movie_catalog_api.service.MovieService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController // это REST API контроллер, Он автоматически возвращает JSON.
+@RequestMapping("/movies") // Базовый URL. Все методы будут начинаться с "/movies"
+
+
+public class MovieController {
+
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping // Получить все фильмы
+    public List<Movie> getAllMovies() {
+        return movieService.getAllMovies();
+    }
+
+    @GetMapping("/{id}") // Получить фильм по ID
+    public Movie getMovieById(@PathVariable Long id) {
+        return movieService.getMovieById(id);
+    }
+
+    @PostMapping // Создать фильм
+    public Movie createMovie(@RequestBody Movie movie) {
+        return movieService.saveMovie(movie);
+    }
+
+    @DeleteMapping("/{id}") // Удалить фильм
+    public void deleteMovie(@PathVariable Long id) {
+        movieService.deleteMovie(id);
+    }
+}
