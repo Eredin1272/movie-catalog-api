f4e5b56306c0e47f761d5672798b3e16052764b5
Eredin <britcovegor@gmail.com>
2026-03-10 12:46:21 +0200

    "for Vadick and Serij only"

--- src/main/java/com/moviesCatalog/movie_catalog_api/service/MovieService.java
+++ src/main/java/com/moviesCatalog/movie_catalog_api/service/MovieService.java
@@ -1,23 +1,18 @@
 package com.moviesCatalog.movie_catalog_api.service;
 
-import com.moviesCatalog.movie_catalog_api.model.Actor;
 import com.moviesCatalog.movie_catalog_api.model.Movie;
-import com.moviesCatalog.movie_catalog_api.repository.ActorRepository;
 import com.moviesCatalog.movie_catalog_api.repository.MovieRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service // аннотация, показывающая что это сервисный компонент
 public class MovieService {
 
     private final MovieRepository movieRepository; // Это ссылка на Repository, который работает с базой данных.
-    private final ActorRepository actorRepository;
     // Конструктор
-    public MovieService(MovieRepository movieRepository, ActorRepository actorRepository) {
+    public MovieService(MovieRepository movieRepository) {
         this.movieRepository = movieRepository;
-        this.actorRepository = actorRepository;
     } // Spring автоматически передаст Repository - Dependency Injection
 
     public List<Movie> getAllMovies() {
@@ -29,17 +24,6 @@ public class MovieService {
     } // SELECT * FROM movies WHERE id = ?
 
     public Movie saveMovie(Movie movie) {
-        List<Actor> actorsFromDb = new ArrayList<>();
-
-        for (Actor actor : movie.getActors()) {
-
-            Actor dbActor = actorRepository.findById(actor.getId())
-                    .orElseThrow(() -> new RuntimeException("Actor not found"));
-
-            actorsFromDb.add(dbActor);
-        }
-
-        movie.setActors(actorsFromDb);
         return movieRepository.save(movie);
     } // INSERT INTO movies ...
 
